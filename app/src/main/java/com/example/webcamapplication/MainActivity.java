661521afe5f3fc78441288bf32eba1b6283c7a73
evgeny12712 <73851525+evgeny12712@users.noreply.github.com>
2020-11-05 20:13:34 +0200

    Revert "com123"
    This reverts commit 370fc2ee4df791d7dfb63d41c9fb8c7e28758536.

--- app/src/main/java/com/example/webcamapplication/MainActivity.java
+++ app/src/main/java/com/example/webcamapplication/MainActivity.java
@@ -56,12 +56,8 @@ public class MainActivity extends AppCompatActivity {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
             camera.connectCamera(cameraManager, isPermission, backgroundHandlerThread, camera.getCameraId());
             Toast.makeText(getApplicationContext(), "CAMERA READY", Toast.LENGTH_SHORT).show();
-=======
-            connectCamera();
->>>>>>> parent of 5f020f4... cameraClass
 =======
             connectCamera();
 >>>>>>> parent of 5f020f4... cameraClass
@@ -193,12 +189,8 @@ public class MainActivity extends AppCompatActivity {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
             camera.connectCamera(cameraManager, isPermission, backgroundHandlerThread, camera.getCameraId());
             startPreview();
-=======
-            connectCamera();
->>>>>>> parent of 5f020f4... cameraClass
 =======
             connectCamera();
 >>>>>>> parent of 5f020f4... cameraClass
